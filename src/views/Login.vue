2c15c157b157ce1b7f5ab7f4479360aef21e66b1
a9043 <zzz13129180808@gmail.com>
2019-01-02 14:09:07 +0800

    safari

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -46,12 +46,12 @@
                     puId: [{
                         required: true,
                         message: '用户名不能为空',
-                        trigger: 'blur'
+                        trigger: 'change'
                     }],
                     puPassword: [{
                         required: true,
                         message: '密码不能为空',
-                        trigger: 'blur'
+                        trigger: 'change'
                     }]
                 },
                 loginProcessing: false,
